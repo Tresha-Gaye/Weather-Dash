eec36d1b0f1cad44ec3ccb030f07156c97afcd90
Twigster <treshagaye@gmail.com>
2022-01-01 21:08:05 -0500

    added new scripting to html and js files to test api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,70 @@
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputValue");
+var name = document.querySelector(".name");
+var description = document.querySelector(".description");
+var temperature = document.querySelector(".temperature");
+// var cityCallUrl = "api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=d68ab89bff9a1e94c3d51494c09fbe5d";
 
-
-function getWeatherData(cityName) {
-    var key = "d68ab89bff9a1e94c3d51494c09fbe5d";
-    var cityNameUrl = "https://api.openweathermap.org/data/2.5/weather?id= + cityName + '&appid=' + key";
-    
-    
-    fetch("cityNameUrl").then(function(response) {
-        // request was successful
+button.addEventListener("click", function() {
+    fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d68ab89bff9a1e94c3d51494c09fbe5d')
+    .then(function(response) {
+        // call was successful
         if(response.ok) {
             response.json().then(function(data) {
-            console.log(data, city);
-                    });
+            console.log(data);
+            });
         } else {
             alert("Error: City not found.");
         }
-        });
-        };
+    });
+});
+      
 
-getWeatherData();
+            // .catch(err => alert ("Wrong city name!"))
+    
+    // http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+
+
+// function getWeatherData(cityName) {
+//     var key = "d68ab89bff9a1e94c3d51494c09fbe5d";
+//     var cityNameUrl = "https://api.openweathermap.org/data/2.5/weather?id= + cityName + '&appid=' + key";
+    
+    
+//     fetch("cityNameUrl").then(function(response) {
+//         // request was successful
+//         if(response.ok) {
+//             response.json().then(function(data) {
+//             console.log(data, city);
+//                     });
+//         } else {
+//             alert("Error: City not found.");
+//         }
+//         });
+//         };
+
+// getWeatherData();
   
 
-function drawWeather( d ) {
-	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+// function drawWeather( d ) {
+// 	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+// 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
-	document.getElementById('description').innerHTML = d.weather[0].description;
-	document.getElementById('temp').innerHTML = celcius + '&deg;';
-	document.getElementById('location').innerHTML = d.name;
-}
+// 	document.getElementById('description').innerHTML = d.weather[0].description;
+// 	document.getElementById('temp').innerHTML = celcius + '&deg;';
+// 	document.getElementById('location').innerHTML = d.name;
+// }
 
-function weatherBalloon( cityID ) {
-	var key = '{yourkey}';
-	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
-	.then(function(resp) { return resp.json() }) // Convert data to json
-	.then(function(data) {
-		drawWeather(data); // Call drawWeather
-	})
-	.catch(function() {
-		// catch any errors
-	});
-}
+// function weatherBalloon( cityID ) {
+// 	var key = '{yourkey}';
+// 	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+// 	.then(function(resp) { return resp.json() }) // Convert data to json
+// 	.then(function(data) {
+// 		drawWeather(data); // Call drawWeather
+// 	})
+// 	.catch(function() {
+// 		// catch any errors
+// 	});
+// }
 
 
 // var apiKey = d68ab89bff9a1e94c3d51494c09fbe5d;
@@ -67,21 +92,21 @@ function weatherBalloon( cityID ) {
 //   getWeatherData();
 
 
-  // Add click function to save user input in text area to localStorage
-  saveClickerEl.click(function(event) {
-    event.preventDefault();
-    console.log($(this).siblings("textarea").val()); // 'this' points to the button element, siblings are in the same div- need to get the input in textarea
-    console.log($(this).attr("id")); // finds the id attribute in the button element
-    localStorage.setItem($(this).attr("id"), $(this).siblings("textarea").val());
- });
+//   // Add click function to save user input in text area to localStorage
+//   saveClickerEl.click(function(event) {
+//     event.preventDefault();
+//     console.log($(this).siblings("textarea").val()); // 'this' points to the button element, siblings are in the same div- need to get the input in textarea
+//     console.log($(this).attr("id")); // finds the id attribute in the button element
+//     localStorage.setItem($(this).attr("id"), $(this).siblings("textarea").val());
+//  });
 
 
-// function to get user inputs saved in localStorage so they persist after a refresh
-for (var i = 9; i < 18; i++) { 
- console.log(localStorage.getItem(i)); // i = id of the button element (#9, #10, #11 etc)
- console.log($("#due-" + i)); // due-x is the id of the textarea
- $("#due-" + i).val(localStorage.getItem(i));
-};
+// // function to get user inputs saved in localStorage so they persist after a refresh
+// for (var i = 9; i < 18; i++) { 
+//  console.log(localStorage.getItem(i)); // i = id of the button element (#9, #10, #11 etc)
+//  console.log($("#due-" + i)); // due-x is the id of the textarea
+//  $("#due-" + i).val(localStorage.getItem(i));
+// };
 
 //   - search for city, write city name, enter, current weather + 5 day forecast
 
@@ -91,4 +116,4 @@ for (var i = 9; i < 18; i++) {
 
 // click back ona  city in recent search and get the 5 day forecast
 
-//  local storage savce as an array
+//  local storage save as an array
