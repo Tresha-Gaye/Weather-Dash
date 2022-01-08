740d28841bc1f14388200a683206ba6c7f36abf3
Twigster <treshagaye@gmail.com>
2022-01-08 11:58:33 -0500

    added function to display 5 day forecast on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,17 +57,24 @@ var fiveDayForecast = function(lat, lon) {
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
-            var uviEl = document.getElementById("uv-index");
+            var uviEl = document.getElementById("uv-index"); 
             // var uvNum = data.current.uvi;
-            // var uvNumEl = document.createElement("span")
+            // var uvNumEl = document.createElement("span")      // how do i style the uvi textContent?
             // uvNumEl.classList = "badge bg-success";
             // uvNumEl.appendChild(uviEl);
             // uvNum = document.getElementById("uv-span"); 
             uviEl.textContent = "UV Index: " + data.current.uvi;
-            var dayOneEl = document.getElementById("day-1");
-            var date = new Date(data.daily[1].dt * 1000);
-            dayOneEl.textContent = date;
-            console.log(date);
+            // var dayOneEl = document.getElementById("day-1");
+            // var unixDate = data.daily[1].dt;
+            // var milliSec = unixDate * 1000;                 // how do i convert the date from unix?
+            // var dateObj = new Date(milliSec);
+            // var formatDate = dateObj.toLocaleTimeString("en-US", {timeZoneName: "short"});
+            //     console.log(formatDate);
+            // var date = new Date(data.daily[1].dt * 1000);
+            // dayOneEl.textContent =  data.daily[1].dt;
+            // console.log(date);
+
+            // DAY 1-TOMORROW'S FORECAST                                          // is there a way to iterate these so I dont have to have 5 separate functions?  
             var temp1El = document.getElementById("temp-1");
             temp1El.textContent = "Temp: " + data.daily[1].temp.day + "℉";
             var wind1El = document.getElementById("wind-1");
@@ -75,7 +82,39 @@ var fiveDayForecast = function(lat, lon) {
             var humid1El = document.getElementById("humid-1");
             humid1El.textContent = "Humidity: " + data.daily[1].humidity + " %";
             
+            // DAY 2 FORECAST
+            var temp2El = document.getElementById("temp-2");
+            temp2El.textContent = "Temp: " + data.daily[2].temp.day + "℉";
+            var wind2El = document.getElementById("wind-2");
+            wind2El.textContent = "Wind: " + data.daily[2].wind_speed + " MPH";
+            var humid2El = document.getElementById("humid-2");
+            humid2El.textContent = "Humidity: " + data.daily[2].humidity + " %";
 
+            // DAY 3 FORECAST
+            var temp3El = document.getElementById("temp-3");
+            temp3El.textContent = "Temp: " + data.daily[3].temp.day + "℉";
+            var wind3El = document.getElementById("wind-3");
+            wind3El.textContent = "Wind: " + data.daily[3].wind_speed + " MPH";
+            var humid3El = document.getElementById("humid-3");
+            humid3El.textContent = "Humidity: " + data.daily[3].humidity + " %";
+
+            // DAY 4 FORECAST
+            var temp4El = document.getElementById("temp-4");
+            temp4El.textContent = "Temp: " + data.daily[4].temp.day + "℉";
+            var wind4El = document.getElementById("wind-4");
+            wind4El.textContent = "Wind: " + data.daily[4].wind_speed + " MPH";
+            var humid4El = document.getElementById("humid-4");
+            humid4El.textContent = "Humidity: " + data.daily[4].humidity + " %";
+
+            // DAY 5 FORECAST
+            // var day5El = document.getElementById("day-5");
+            // day5El.textContent =  data.daily[5].dt;
+            var temp5El = document.getElementById("temp-5");
+            temp5El.textContent = "Temp: " + data.daily[5].temp.day + "℉";
+            var wind5El = document.getElementById("wind-5");
+            wind5El.textContent = "Wind: " + data.daily[5].wind_speed + " MPH";
+            var humid5El = document.getElementById("humid-5");
+            humid5El.textContent = "Humidity: " + data.daily[5].humidity + " %";
             }
         )}
     })
